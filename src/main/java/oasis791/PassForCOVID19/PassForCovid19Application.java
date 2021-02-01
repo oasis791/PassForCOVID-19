0eedbf52ca80174f8e892e71a2100b097fb11801
oasis791 <khwi791@naver.com>
2021-02-01 17:13:47 +0900

    Initial Commit

---
+++ src/main/java/oasis791/PassForCOVID19/PassForCovid19Application.java
@@ -0,0 +1,13 @@
+package oasis791.PassForCOVID19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassForCovid19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassForCovid19Application.class, args);
+	}
+
+}
---
+++ src/test/java/oasis791/PassForCOVID19/PassForCovid19ApplicationTests.java
@@ -0,0 +1,13 @@
+package oasis791.PassForCOVID19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassForCovid19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
